6b70697c1956a966aa7d58d9849f99bc2e18e409
suzanhadzhieva <suzan.hadzhieva@paysafe.com>
2023-09-20 12:13:33 +0300

    Resolve conflicts

--- src/prediction/pipeline/slack_sender.py
+++ src/prediction/pipeline/slack_sender.py
@@ -1,5 +1,5 @@
-from prediction import logger
-from prediction.config.configuration import ConfigManager
+from src.prediction import logger
+from src.prediction.config.configuration import ConfigManager
 from slack_sdk import WebClient
 import os
 
